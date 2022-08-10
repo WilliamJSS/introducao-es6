bd6f9b8ed0614069ec06368933abe8d1d4de8470
WilliamJSS <williamsena088@gmail.com>
2022-08-10 07:20:11 -0300

    Metodos estaticos e tipos de variaveis

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ class List {
     }
 }
 
+// Classe com Herança
+
 class ToList extends List {
     constructor(){
         super()
@@ -18,6 +20,10 @@ class ToList extends List {
     getUsuario(){
         console.log(this.usuario)
     }
+
+    static soma(a,b){
+        return a+b
+    }
 }
 
 const tolist = new ToList()
@@ -26,3 +32,44 @@ document.getElementById('novotodo').onclick = function () {
     tolist.add('will')
     tolist.getUsuario()
 }
+
+/* Usando de métodos estáticos */
+
+console.log('Soma com metodo estatico:')
+console.log(ToList.soma(1,2))
+
+/* Tipos de variaveis */
+
+// Não pode ser modificada
+const nome = 'Will' 
+
+// Variavel global
+var glob = 10 
+
+// É visível apenas no escopo da função em que foi definida
+let loc = 2
+
+/* Exemplo com variavel const */
+
+const user = {nome: 'Will', idade: 20}
+
+// É possível alterar o valor de uma propriedade, mas nao a estrutura de 'user'
+console.log(user)
+user.idade = 21
+console.log(user)
+
+/* Exemplo com variavel local */
+
+function teste(x){
+    let y = 4
+    if(x>2){
+        y = 6
+    }
+    console.log('dentro da função teste, y:')
+    console.log(y)
+} 
+
+teste(5)
+
+console.log('fora da função teste, y:')
+console.log(y)
