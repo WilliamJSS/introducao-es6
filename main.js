ef8298bb8382cb1137d4a28c897dbc56df875daf
WilliamJSS <williamsena088@gmail.com>
2022-08-12 07:31:23 -0300

    Desestruturação, rest e spread

--- main.js
+++ main.js
@@ -1,59 +1,117 @@
-const arr = [1,2,3,4,6,8,9]
+/* Desestruturação */
 
-/* Função map - mapeia todos os elementos do array, podendo realizar
-operações sobre cada um deles */
-
-const newArr = arr.map(function(item){
-    return item * 2
-})
-
-console.log(arr)
-console.log(newArr)
-
-/* Função reduce - reduz o array a um unico elemento, resultante da
-iteração de todos os elementos do array */
-const soma = arr.reduce(function(total, next){
-    return total + next
-})
-
-console.log(soma)
-
-/* Função filter - filtra os elementos do array, mantendo apenas
-os que forem de acordo com a condição definida */
-
-// retorna os elementos pares do array
-const filter = arr.filter(function(item){
-    return item % 2 == 0
-})
-
-console.log(filter)
-
-/* Função find - busca um elemento no array */
-
-// retorna o elemento ou 'undefined' caso não o encontre
-const find = arr.find(function(item){
-    return item === 7
-})
-
-console.log(find)
-
-/* Arrow Functions - diminui a verbosidade do código */
-
-// representa o mesmo map efetuado no início do código
-const arrowArr = arr.map((item) => item*2)
-console.log(arrowArr)
-
-const teste = () => {
-    return {nome: 'Will'}
-}
-console.log(teste())
-
-/* Parametros default */
-
-function somador(a=2, b=3){
-    return a+b
+const usuario = {
+    nome: 'William',
+    idade: 21,
+    endereco: {
+        estado: 'BA',
+        cidade: 'Jequié'
+    }
 }
 
-console.log(somador(6,5))
-console.log(somador(3))
-console.log(somador())
+console.log(usuario)
+
+// Sem desestruturação
+function semDesestruturacao(usuario){
+    const nome = usuario.nome
+    const idade = usuario.idade
+    
+    console.log(nome)
+    console.log(idade)
+}
+
+semDesestruturacao(usuario)
+
+// Com desestruturação
+function comDesestruturacao(usuario){
+    
+    const {nome, idade, endereco:{cidade}} = usuario
+    
+    console.log(nome)
+    console.log(idade)
+    console.log(cidade)
+}
+
+comDesestruturacao(usuario)
+
+function getUserName(usuario){
+    console.log(usuario.nome)
+}
+
+function getName({nome}){
+    console.log(nome)
+}
+
+getUserName(usuario)
+getName(usuario)
+
+/* Rest */
+
+function rest(usuario){
+    const {nome, ...resto} = usuario
+
+    console.log(resto)
+}
+
+rest(usuario)
+
+function arrRest(){
+    const arr = [1,2,3,4,5]
+    
+    const [a,b,...c] = arr
+    
+    console.log(c)
+}
+
+arrRest()
+
+function soma(...params){
+    return params.reduce((total,next)=>total+next)
+}
+
+console.log(soma(1))
+console.log(soma(1,2))
+console.log(soma(10,10,10))
+console.log(soma(0))
+
+/* Spread */
+
+const usuario1 = {
+    nome: 'Will',
+    idade: 21,
+    curso: 'SI'
+}
+
+const usuario2 = usuario1
+
+const usuario3 = {...usuario1, idade: 25}
+
+console.log(usuario1)
+console.log(usuario2)
+console.log(usuario3)
+
+/* Templates */
+
+const nome = 'Will'
+const idade = 21
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos')
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+
+/* Sintaxe curta de objetos */
+
+const usuarioNormal = {
+    nome: nome,
+    idade: idade,
+    curso: 'SI'
+}
+
+const usuarioCurto = {
+    nome,
+    idade,
+    curso: 'SI'
+}
+
+console.log(usuarioNormal)
+console.log(usuarioCurto)
